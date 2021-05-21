1afeb6e110a60352f5a0bee48c7d230f8ad51e9b
jjhstoday <jjh.s.today@gmail.com>
2021-05-21 17:38:49 +0900

    feat: Scroll-Spy resize 버그 수정

--- case2_ScrollSpy/question/q2_js_2/index.js
+++ case2_ScrollSpy/question/q2_js_2/index.js
@@ -1,22 +1,45 @@
-import "./style.css";
-
-const navElem = document.querySelector("#nav");
+const navElem = document.querySelector('#nav');
 const navItems = Array.from(navElem.children);
-const contentsElem = document.querySelector("#contents");
+const contentsElem = document.querySelector('#contents');
 const contentItems = Array.from(contentsElem.children);
-const getOffsetTops = null; // do something
+const getOffsetTops = (() => {
+  let ofset = 0;
+  let res = [];
 
-window.addEventListener("scroll", e => {
+  return () => {
+    if (window.innerHeight === ofset) {
+      return res;
+    }
+    ofset = window.innerHeight;
+    res = contentItems.map(elem => {
+      const [ofs, clh] = [elem.offsetTop, elem.clientHeight];
+      return [ofs - clh / 2, ofs + clh / 2];
+    });
+    return res;
+  };
+})();
+
+window.addEventListener('scroll', e => {
   // fix here
+  const { scrollTop } = e.target.scrollingElement;
+
+  const targetIndex = getOffsetTops().findIndex(
+    ([from, to]) => scrollTop >= from && scrollTop < to
+  );
+  console.log(targetIndex);
+  Array.from(navElem.children).forEach((c, i) => {
+    if (i !== targetIndex) c.classList.remove('on');
+    else c.classList.add('on');
+  });
 });
 
-navElem.addEventListener("click", e => {
+navElem.addEventListener('click', e => {
   const targetElem = e.target;
-  if (targetElem.tagName === "BUTTON") {
+  if (targetElem.tagName === 'BUTTON') {
     const targetIndex = navItems.indexOf(targetElem.parentElement);
     contentItems[targetIndex].scrollIntoView({
-      block: "start",
-      behavior: "smooth"
+      block: 'start',
+      behavior: 'smooth'
     });
   }
 });
