38200dda7640e03cb4d7d19e7f6a9ce382375a13
jjhstoday <jjh.s.today@gmail.com>
2021-05-21 17:51:03 +0900

    feat: Scroll-Spy resize listener 적용하여 동작 구현

--- case2_ScrollSpy/question/q3_js_resize_listener/index.js
+++ case2_ScrollSpy/question/q3_js_resize_listener/index.js
@@ -1,36 +1,39 @@
-import "./style.css";
-
-const navElem = document.querySelector("#nav");
+const navElem = document.querySelector('#nav');
 const navItems = Array.from(navElem.children);
-const contentsElem = document.querySelector("#contents");
+const contentsElem = document.querySelector('#contents');
 const contentItems = Array.from(contentsElem.children);
 
 let offsetTops = [];
 const getOffsetTops = () => {
   // do something
+  offsetTops = contentItems.map(elem => {
+    const [ofs, clh] = [elem.offsetTop, elem.clientHeight];
+    return [ofs - clh / 2, ofs + clh / 2];
+  });
 };
 getOffsetTops();
 
-window.addEventListener("scroll", e => {
+window.addEventListener('scroll', e => {
   const { scrollTop } = e.target.scrollingElement;
-  const targetIndex = offsetTops.findIndex(([from, to]) => (
-    scrollTop >= from && scrollTop < to
-  ))
+  const targetIndex = offsetTops.findIndex(
+    ([from, to]) => scrollTop >= from && scrollTop < to
+  );
+  console.log(targetIndex);
   Array.from(navElem.children).forEach((c, i) => {
     if (i !== targetIndex) c.classList.remove('on');
     else c.classList.add('on');
   });
 });
 
-window.addEventListener("resize", getOffsetTops);
+window.addEventListener('resize', getOffsetTops);
 
-navElem.addEventListener("click", e => {
+navElem.addEventListener('click', e => {
   const targetElem = e.target;
-  if (targetElem.tagName === "BUTTON") {
+  if (targetElem.tagName === 'BUTTON') {
     const targetIndex = navItems.indexOf(targetElem.parentElement);
     contentItems[targetIndex].scrollIntoView({
-      block: "start",
-      behavior: "smooth"
+      block: 'start',
+      behavior: 'smooth'
     });
   }
 });
