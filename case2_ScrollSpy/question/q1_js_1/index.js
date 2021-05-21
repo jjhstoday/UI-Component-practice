55b8403175bda8eca3be825f48dc8b36f30e897a
jjhstoday <jjh.s.today@gmail.com>
2021-05-21 17:25:03 +0900

    feat: Scroll-Spy 기본 구현

--- case2_ScrollSpy/question/q1_js_1/index.js
+++ case2_ScrollSpy/question/q1_js_1/index.js
@@ -1,27 +1,31 @@
-import "./style.css";
-
-const navElem = document.querySelector("#nav");
+const navElem = document.querySelector('#nav');
 const navItems = Array.from(navElem.children);
-const contentsElem = document.querySelector("#contents");
+const contentsElem = document.querySelector('#contents');
 const contentItems = Array.from(contentsElem.children);
-const offsetTops = contentItems.map((elem) => {
+const offsetTops = contentItems.map(elem => {
   const [ofs, clh] = [elem.offsetTop, elem.clientHeight];
   return [ofs - clh / 2, ofs + clh / 2];
 });
 
-window.addEventListener("scroll", (e) => {
+window.addEventListener('scroll', e => {
   const { scrollTop } = e.target.scrollingElement;
   // do something
+  const targetIndex = offsetTops.findIndex(
+    ([from, to]) => scrollTop >= from && scrollTop < to
+  );
+  Array.from(navElem.children).forEach((c, i) => {
+    if (i !== targetIndex) c.classList.remove('on');
+    else c.classList.add('on');
+  });
 });
 
-navElem.addEventListener("click", (e) => {
+navElem.addEventListener('click', e => {
   const targetElem = e.target;
-  if (targetElem.tagName === "BUTTON") {
+  if (targetElem.tagName === 'BUTTON') {
     const targetIndex = navItems.indexOf(targetElem.parentElement);
     contentItems[targetIndex].scrollIntoView({
-      block: "start",
-      behavior: "smooth",
+      block: 'start',
+      behavior: 'smooth'
     });
   }
 });
-
