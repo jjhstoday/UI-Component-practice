716bba86b1e09624bd4bf97fe50a3aaae2c99bce
jjhstoday <jjh.s.today@gmail.com>
2021-05-31 17:13:24 +0900

    feat: Analog-Clock 기능 구현 with JS

--- case7_analog-clock/question/1.js/index.js
+++ case7_analog-clock/question/1.js/index.js
@@ -1,17 +1,28 @@
-// 요구 사항은 다음과 같다.
-
-// 1. 시계의 시침(.hand.hour 요소), 분침(.hand.minute 요소), 초침(.hand.second 요소)을
-// 1초 간격으로 회전시켜 현재 시간을 표시한다.
-
 const renderTime = (() => {
-    const $hourHand = document.querySelector('.hand.hour');
-    const $minuteHand = document.querySelector('.hand.minute');
-    const $secondHand = document.querySelector('.hand.second');
+  // 한번만 실행된다.
+  const $hourHand = document.querySelector('.hand.hour');
+  const $minuteHand = document.querySelector('.hand.minute');
+  const $secondHand = document.querySelector('.hand.second');
 
-    //Write JS code here!
+  return () => {
+    const now = new Date();
 
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    // 초침: 1초당 6도
+    $secondHand.style.setProperty('--deg', seconds * 6);
+    // 분침: 1분당 6도, 1초당 0.1도(6/60)
+    $minuteHand.style.setProperty('--deg', minutes * 6 + seconds * 0.1);
+    // 시침: 1시간당 30도, 1분당 0.5도(30/60), 1초당 (0.5/60)
+    $hourHand.style.setProperty(
+      '--deg',
+      hours * 30 + minutes * 0.5 + seconds * (0.5 / 60)
+    );
+  };
 })();
 
 document.addEventListener('DOMContentLoaded', () => {
-    //Write JS code here!
+  setInterval(renderTime, 1000);
 });
