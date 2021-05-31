b77daf8dd791a9b1f207666048f2a8dc46f5f4bd
jjhstoday <jjh.s.today@gmail.com>
2021-05-31 17:44:26 +0900

    feat: Analog-Clock UI 구현 with React

--- case7_analog-clock/question/2.react-styled-component/src/App.js
+++ case7_analog-clock/question/2.react-styled-component/src/App.js
@@ -1,28 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from './styles/global';
+import Title from './components/Title';
+import AnalogClock from './components/AnalogClock';
 
 function App() {
   return (
     <>
-    <h1 class="title">Analog clock</h1>
-    <div class="clock">
-
-      <div class="hand hour"></div>
-      <div class="hand minute"></div>
-      <div class="hand second"></div>
-      <div class="time time1">|</div>
-      <div class="time time2">|</div>
-      <div class="time time3">|</div>
-      <div class="time time4">|</div>
-      <div class="time time5">|</div>
-      <div class="time time6">|</div>
-      <div class="time time7">|</div>
-      <div class="time time8">|</div>
-      <div class="time time9">|</div>
-      <div class="time time10">|</div>
-      <div class="time time11">|</div>
-      <div class="time time12">|</div>
-    </div>
+      <GlobalStyles />
+      <Title>Analog clock</Title>
+      <AnalogClock />
     </>
   );
 }
